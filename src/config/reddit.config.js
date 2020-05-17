24cbefa0733abbb14807820385c66636562ce856
Mate Balo Bordas <mate.balob@gmail.com>
2020-05-17 11:19:38 +0100

    add reddit config

---
+++ src/config/reddit.config.js
@@ -0,0 +1,10 @@
+const redditConfig = {
+  appId: 'khPBf6R-M0yLkw',
+  responseType: 'token',
+  duration: 'permanent',
+  redirectURI: 'http://localhost:3000/home',
+};
+
+export {
+  redditConfig,
+};
