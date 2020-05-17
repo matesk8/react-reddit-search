4998ecfa6afd3a63e3c4049334ee1765d1497fd9
Mate Balo Bordas <mate.balob@gmail.com>
2020-05-17 14:19:39 +0100

    run eslint:fix on the localStorage.util.js

--- src/utils/localStorage.util.js
+++ src/utils/localStorage.util.js
@@ -2,9 +2,7 @@ const setLocalStorageValue = (key, value) => {
   window.localStorage.setItem(key, JSON.stringify(value));
 };
 
-const getLocalStorageValue = (key) => {
-  return window.localStorage.getItem(key);
-};
+const getLocalStorageValue = (key) => window.localStorage.getItem(key);
 
 export {
   setLocalStorageValue,
