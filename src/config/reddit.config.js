9a489c8d3c47fbcd3039657999828d990484d51a
Mate Balo Bordas <mate.balob@gmail.com>
2020-05-19 12:04:50 +0100

    add README.md

--- src/config/reddit.config.js
+++ src/config/reddit.config.js
@@ -2,7 +2,7 @@ const redditConfig = {
   appId: 'khPBf6R-M0yLkw',
   responseType: 'token',
   duration: 'permanent',
-  redirectURI: 'http://localhost:3000/home',
+  redirectURI: 'http://localhost:3000/home', // TODO: change this if needed
 };
 
 export {
