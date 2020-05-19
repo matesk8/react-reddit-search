c2f1c725e7580079962ee79840de0b50b8a14700
Mate Balo Bordas <mate.balob@gmail.com>
2020-05-19 11:37:40 +0100

    add util for create time text

--- src/utils/time.util.js
+++ src/utils/time.util.js
@@ -1,3 +1,6 @@
+const moment = require('moment');
+require('moment-precise-range-plugin');
+
 const getYearDifferenceFromNow = (date) => {
   const difference = Date.now() - date;
   const differenceDate = new Date(difference);
@@ -7,13 +10,36 @@ const getYearDifferenceFromNow = (date) => {
 
 const unixTimeStampToMilliseconds = (unixTimeStamp) => new Date(unixTimeStamp * 1000);
 
-const getMinutesDifference = (date1, date2) => {
-  const difference = date1 - date2;
-  return Math.abs(Math.round(((difference % 86400000) % 3600000) / 60000));
+const getCreatedTimeAgoText = (date1, date2) => {
+  const momentDate1 = moment(date1);
+  const momentDate2 = moment(date2);
+  const difference = moment.preciseDiff(momentDate1, momentDate2, true);
+  const {
+    minutes, hours, days, months, years
+  } = difference;
+  const yearText = years > 1 ? 'years' : 'year';
+  const monthsText = months > 1 ? 'months' : 'month';
+  const daysText = days > 1 ? 'days' : 'day';
+  const hoursText = hours > 1 ? 'hours' : 'hour';
+  const minutesText = minutes > 1 ? 'minutes' : 'minute';
+
+  if (years >= 1) {
+    return `${years} ${yearText} ago`;
+  }
+  if (months >= 1) {
+    return `${months} ${monthsText} ago`;
+  }
+  if (days >= 1) {
+    return `${days} ${daysText} ago`;
+  }
+  if (hours >= 1) {
+    return `${hours} ${hoursText} ago`;
+  }
+  return `${minutes} ${minutesText} ago`;
 };
 
 export {
   getYearDifferenceFromNow,
   unixTimeStampToMilliseconds,
-  getMinutesDifference,
+  getCreatedTimeAgoText,
 };
--- src/utils/time.util.spec.js
+++ src/utils/time.util.spec.js
@@ -1,24 +1,38 @@
-import { getMinutesDifference } from './time.util';
+import { getCreatedTimeAgoText } from './time.util';
 
-describe('getMinutesDifference', () => {
-  test('should return 5 for 5 minute difference', () => {
+describe('getCreatedTimeAgoText', () => {
+  test('should return 5 minutes ago for 5 minute difference', () => {
     const date1 = new Date('1989-09-04T03:20:00');
     const date2 = new Date('1989-09-04T03:25:00');
-    const result = getMinutesDifference(date1, date2);
-    expect(result).toEqual(5);
+    const result = getCreatedTimeAgoText(date1, date2);
+    expect(result).toEqual('5 minutes ago');
   });
 
-  test('should return 0 for 0 minute difference', () => {
+  test('should return 1 hour ago for 1 hour difference', () => {
     const date1 = new Date('1989-09-04T03:20:00');
-    const date2 = new Date('1989-09-04T03:20:00');
-    const result = getMinutesDifference(date1, date2);
-    expect(result).toEqual(0);
+    const date2 = new Date('1989-09-04T02:20:00');
+    const result = getCreatedTimeAgoText(date1, date2);
+    expect(result).toEqual('1 hour ago');
   });
 
-  test('should return 15 for 15 minute difference', () => {
-    const date1 = new Date('1989-09-04T03:55:00');
-    const date2 = new Date('1989-09-04T04:10:00');
-    const result = getMinutesDifference(date1, date2);
-    expect(result).toEqual(15);
+  test('should return 15 days ago for 15 days difference', () => {
+    const date1 = new Date('1989-01-01T03:55:00');
+    const date2 = new Date('1989-01-16T04:10:00');
+    const result = getCreatedTimeAgoText(date1, date2);
+    expect(result).toEqual('15 days ago');
+  });
+
+  test('should return 1 month ago 1 for month difference', () => {
+    const date1 = new Date('1989-01-01T03:55:00');
+    const date2 = new Date('1989-02-16T03:10:00');
+    const result = getCreatedTimeAgoText(date1, date2);
+    expect(result).toEqual('1 month ago');
+  });
+
+  test('should return 2 years ago for 2 years difference', () => {
+    const date1 = new Date('1989-01-01T03:55:00');
+    const date2 = new Date('1991-03-16T03:10:00');
+    const result = getCreatedTimeAgoText(date1, date2);
+    expect(result).toEqual('2 years ago');
   });
 });
